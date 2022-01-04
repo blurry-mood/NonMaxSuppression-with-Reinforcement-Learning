ba996571619a8971e24cbbf1d7abcd4fe98e6f65
blurry-mood <assis.ayoub@gmail.com>
2022-01-04 01:51:53 +0100

    minor cleanup

--- src/validate_model.py
+++ src/validate_model.py
@@ -18,9 +18,12 @@ parser = ArgumentParser(
     description="This script visualizes the WIDER Face dataset samples one at a time. To view the next image type `n` (next), and to quit type `q` (quit).")
 parser.add_argument('--split', type=str, default='train', choices=['train', 'val', 'test'],
                     help='Which dataset split to visualize: `train`, `val`, and `test`')
-parser.add_argument('--imgsz', type=int, default='1024', help='Specify the image size (width=height)')
-parser.add_argument('--conf-thres', type=float, default='0.5', help='Specify the confidence thresold for NMS')
-parser.add_argument('--iou-thres', type=float, default='0.5', help='Specify the IoU thresold for NMS')
+parser.add_argument('--imgsz', type=int, default='1024',
+                    help='Specify the image size (width=height)')
+parser.add_argument('--conf-thres', type=float, default='0.5',
+                    help='Specify the confidence thresold for NMS')
+parser.add_argument('--iou-thres', type=float, default='0.5',
+                    help='Specify the IoU thresold for NMS')
 
 args = parser.parse_args()
 split_set = args.split
@@ -28,25 +31,28 @@ size = args.imgsz
 conf_thres = args.conf_thres
 iou_thres = args.iou_thres
 
-image_paths = glob(join(_CURRENT_DIR, '..', 'dataset', f'WIDER_{split_set}', 'images', '**', '*.jpg'), recursive=True)
+image_paths = glob(join(_CURRENT_DIR, '..', 'dataset',
+                        f'WIDER_{split_set}', 'images', '**', '*.jpg'), recursive=True)
 assert image_paths != [], "We couldn't find any image !"
 print(f"{len(image_paths)} image(s) are found!")
 image_paths.sort()
 
 _model = get_trained_model(size)
 
+
 def get_bboxes(img):
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     img = (img - np.array([104, 117, 123])).transpose(2, 0, 1)
     img = torch.from_numpy(img).float().unsqueeze(0)
-    
+
     boxes, scores = _model(img)
-    
+
     mask = scores > conf_thres
     boxes = boxes[mask]
     scores = scores[mask]
-    
-    inds = batched_nms(boxes, scores, torch.tensor([0 for _ in range(scores.shape[0])]), iou_threshold=iou_thres)
+
+    inds = batched_nms(boxes, scores, torch.tensor(
+        [0 for _ in range(scores.shape[0])]), iou_threshold=iou_thres)
 
     return boxes[inds].int().numpy()
 
@@ -54,18 +60,19 @@ def get_bboxes(img):
 cv.namedWindow("Validation", cv.WINDOW_GUI_NORMAL)
 
 with tqdm(total=len(image_paths)) as pbar:
-    for img_path in image_paths:  
+    for img_path in image_paths:
         # read image
         original = cv.imread(img_path)
         original = cv.resize(original, (size, size))
         img = original.copy()
-        
+
         # read bounding-boxes
         labels = get_bboxes(img)
-        
+
         # show infos
-        pbar.set_postfix({'image':split(img_path)[1], 'n_labels':labels.shape[0]})
-        
+        pbar.set_postfix(
+            {'image': split(img_path)[1], 'n_labels': labels.shape[0]})
+
         # visualize boxes
         for label in labels:
             x1, y1, x2, y2 = label
@@ -90,5 +97,3 @@ with tqdm(total=len(image_paths)) as pbar:
             _exit = True
 
         pbar.update(1)
-
-
