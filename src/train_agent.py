1ff00d71d2d6122ffa3ca9474bfa4cc2301a78b7
blurry-mood <assis.ayoub@gmail.com>
2022-01-17 16:57:39 +0100

    move configuration to config.py, change DQN network

--- src/train_agent.py
+++ src/train_agent.py
@@ -5,25 +5,25 @@ from torch import nn
 import numpy as np
 import torch
 from environment import ModelEnv
-
+from config import *
 from os.path import split, join
 
 _CURRENT_DIR = split(__file__)[0]
 
-ALPHA = 2e-1
-GAMMA = 0.99
-EPS = 1e-1
-ITERS = 2
-
 
 class Model(nn.Module):
 
-    def __init__(self, in_dims, n_actions):
+    def __init__(self):
         super().__init__()
 
-        self.model = nn.Sequential(nn.Linear(in_dims, 64),
+        self.model = nn.Sequential(nn.Conv2d(1, 32, 3, padding=5, dilation=3),
                                    nn.ReLU(),
-                                   nn.Linear(64, n_actions)
+                                   nn.Conv2d(32, 64, 5, padding=5, dilation=3),
+                                   nn.ReLU(),
+                                   nn.AdaptiveAvgPool2d(1),
+                                   nn.Flatten(),
+                                   nn.ReLU(),
+                                   nn.Linear(64, 2)
                                    )
 
     def forward(self, x):
@@ -32,42 +32,44 @@ class Model(nn.Module):
 
 class MyAgent(DQN):
 
-    def decode_state(self, bboxes: np.ndarray):
-        mask = bboxes[:, 5] == 0
-        # bboxes[mask] = 0
-        s = np.delete(bboxes, 5, axis=1)
-        s = torch.from_numpy(s).flatten().unsqueeze(0).float()
+    def decode_state(self, state: torch.Tensor):
+        if state is None:
+            return None
+        s = state.unsqueeze(0).unsqueeze(0).float()
         return s
 
 
-img_size = 128
-
-env = ModelEnv('train', img_size)
-dqn = MyAgent(Model(env.n_boxes * 7, 2), actions=list(range(env.action_space.n)), alpha=ALPHA, gamma=GAMMA, eps=EPS)
+env = ModelEnv('train', IMG_SIZE, CONF_THRES)
+dqn = MyAgent(Model(), actions=list(range(env.action_space.n)), alpha=ALPHA, gamma=GAMMA, eps=EPS)
 dqn.load(join(_CURRENT_DIR, '..', 'artifacts', 'dqn'))
 
-for i in range(ITERS):
+losses = []
+rewards = []
+
+for i in range(EPISODES):
     state = env.reset()
     env.render()
     n = 0
     done = False
-    reds = 0
-    ones = 0
-    with tqdm(desc=f'Episode {i}') as pbar:
+    _loss = 0
+    with tqdm(desc=f'Episode {i + 1}') as pbar:
         while not done:
             n += 1
             action = dqn.take_action(state)
-            ones += action
             state, reward, done, info = env.step(action)
-            reward += 1 / (ones + 1)
-            dqn.update(state, reward)
+
+            loss = dqn.update(state, reward)
+            if loss is not None:
+                _loss += loss
             env.render()
 
-            reds += reward
-
             pbar.update(1)
-            pbar.set_postfix({'reward': reds, 'ones': ones / n})
+            pbar.set_postfix({'reward': reward, 'loss': loss, 'Number of bboxes': env.observation_space.shape})
 
+    dqn.buffer.buffer.clear()
+
+    rewards.append(reward)
+    losses.append(_loss)
     dqn.save(join(_CURRENT_DIR, '..', 'artifacts', 'dqn'))
 
 env.close()
